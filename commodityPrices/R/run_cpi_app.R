77ae25095057cc8d54cc25b6ce5e5e79f70ad999
Rara Soro <60901974+trara538@users.noreply.github.com>
2026-09-16 15:24:46 +1100

    Update run_cpi_app.R

--- commodityPrices/R/run_cpi_app.R
+++ commodityPrices/R/run_cpi_app.R
@@ -5,9 +5,9 @@
 #' @return This function launches a Shiny application.
 #' @export
 
-run_cpi_app <- function() {
+run_commodity_app <- function() {
   
-  app_dir <- system.file("shiny", package = "cpiShiny")
+  app_dir <- system.file("shiny", package = "commodityPricesShiny")
   if (app_dir == ""){
     stop("App directory not found")
   }
