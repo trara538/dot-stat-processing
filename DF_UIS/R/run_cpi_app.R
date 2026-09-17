5c17485759c955d6699cc6478cff11e6fd481f2e
Rara Soro <60901974+trara538@users.noreply.github.com>
2026-09-18 08:32:55 +1100

    Update run_cpi_app.R

--- DF_UIS/R/run_cpi_app.R
+++ DF_UIS/R/run_cpi_app.R
@@ -7,7 +7,7 @@
 
 run_uis_app <- function() {
   
-  app_dir <- system.file("shiny", package = "DF_UIS")
+  app_dir <- system.file("shiny", package = "DFUIS")
   if (app_dir == ""){
     stop("App directory not found")
   }
